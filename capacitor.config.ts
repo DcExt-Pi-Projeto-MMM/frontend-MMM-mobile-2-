ccdf997d59750be9abc3b2a4cbd7fae60f8c3fb3
Hugo Andrade <habp@ecomp.poli.br>
2022-10-01 21:19:26 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'molde_tela_principal',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
