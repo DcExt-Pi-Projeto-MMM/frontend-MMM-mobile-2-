da301071c189e7a74a2c4b6448090f6c865c8c58
Hugo Andrade <habp@ecomp.poli.br>
2022-11-01 09:18:39 -0300

    final update

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,7 @@
-import { IonList, IonImg, IonSelect, IonSelectOption, IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonIcon, IonButton  } from '@ionic/react';
+import { IonList, IonImg, IonSelect, IonSelectOption, IonContent, IonGrid, IonHeader, IonItem, IonCol, IonPage, IonRow, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonIcon, IonButton  } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { pin, wifi, wine, warning, walk, basketOutline, callOutline, personOutline, logoInstagram, logoWhatsapp, atCircleOutline } from 'ionicons/icons';
 
 const Tab2: React.FC = () => {
   return (
@@ -12,7 +13,7 @@ const Tab2: React.FC = () => {
           <IonCardHeader>
             <IonCardTitle>O que deseja vender?</IonCardTitle>
           </IonCardHeader>
-          <IonItem>
+          <IonItem lines='none'>
             <IonSelect placeholder="Produto">
               <IonSelectOption value="alface">Alface</IonSelectOption>
               <IonSelectOption value="arroz">Arroz</IonSelectOption>
@@ -28,23 +29,40 @@ const Tab2: React.FC = () => {
               <IonSelectOption value="macaxeira">Macaxeira</IonSelectOption>
             </IonSelect>
           </IonItem>
-          <IonItem>
+          <IonItem lines='none'>
             <IonSelect placeholder="Quantidade">
-              <IonSelectOption value="alface">2 Kgs</IonSelectOption>
-              <IonSelectOption value="arroz">Arroz</IonSelectOption>
-              <IonSelectOption value="tomate">Tomate</IonSelectOption>
-              <IonSelectOption value="banana">Banana</IonSelectOption>
-              <IonSelectOption value="melancia">Melancia</IonSelectOption>
-              <IonSelectOption value="milho">Milho</IonSelectOption>
-              <IonSelectOption value="feijao">Feijão</IonSelectOption>
-              <IonSelectOption value="abacaxi">Abacaxi</IonSelectOption>
-              <IonSelectOption value="batata">Batata</IonSelectOption>
-              <IonSelectOption value="coentro">Coentro</IonSelectOption>
-              <IonSelectOption value="inhame">Inhame</IonSelectOption>
-              <IonSelectOption value="macaxeira">Macaxeira</IonSelectOption>
+              <IonSelectOption value="um">1</IonSelectOption>
+              <IonSelectOption value="dois">2</IonSelectOption>
+              <IonSelectOption value="tomate">3</IonSelectOption>
+              <IonSelectOption value="quatro">4</IonSelectOption>
+              <IonSelectOption value="cinco">5</IonSelectOption>
+              <IonSelectOption value="seis">6</IonSelectOption>
+              <IonSelectOption value="sete">7</IonSelectOption>
+              <IonSelectOption value="oito">8</IonSelectOption>
+              <IonSelectOption value="nove">9</IonSelectOption>
+              <IonSelectOption value="dez">10</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+          <IonItem lines='none'>
+            <IonSelect placeholder="Medida">
+              <IonSelectOption value="gramas">Gramas</IonSelectOption>
+              <IonSelectOption value="quilos">Quilos</IonSelectOption>
+              <IonSelectOption value="pes">Pés</IonSelectOption>
+              <IonSelectOption value="unidades">Unidades</IonSelectOption>
+              <IonSelectOption value="saca">Saca</IonSelectOption>
+              <IonSelectOption value="arroba">Arroba</IonSelectOption>
             </IonSelect>
           </IonItem>
         </IonCard>
+        <IonGrid>
+          <IonRow className='ion-align-items-center'>
+            <IonCol className='ion-text-center'>
+              <IonButton size='default' color='primary' shape='round'>Vender
+                <IonIcon slot='start' icon={pin}></IonIcon>
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonGrid>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,33 @@
-import { IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonIcon, IonButton  } from '@ionic/react';
+import { IonContent, IonImg, IonHeader, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonIcon, IonButton  } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import { pin, wifi, wine, warning, walk, basketOutline, callOutline, personOutline, logoInstagram, logoWhatsapp, atCircleOutline } from 'ionicons/icons';
+
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+      <IonContent className='ion-padding' fullscreen>
+        <IonCard>
+          <IonImg src='https://marketsplash.com/content/images/2022/08/30-Best-No-Code-Software-Tools-1.png'></IonImg>
+          <IonCardHeader>
+            <IonCardTitle>Meu Perfil</IonCardTitle>
+          </IonCardHeader>
+          <IonItem lines='none' >
+            <IonIcon icon={basketOutline} slot="start" />
+            <IonLabel>Vender</IonLabel>
+          </IonItem>
+          <IonCardContent>
+            Se deseja anunciar e vender sua colheita, clique em vender. 
+          </IonCardContent>
+          <IonItem lines='none' >
+            <IonIcon icon={personOutline} slot="start" />
+            <IonLabel>Minha Conta</IonLabel>
+          </IonItem>
+          <IonCardContent>
+            Aqui você encontra todas as informações relacionadas a sua conta pessoal.
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
